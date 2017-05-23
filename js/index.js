de7d2eeb6e34b8ea4e97d42c1f617845c75aa30f
David Borja <dabor238@gmail.com>
2017-05-23 02:29:07 -0500

    tracking and marketing

--- js/index.js
+++ js/index.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
             $.ajax({
                 type: 'POST',
                 url: 'http://www.resumitelo.com/api/usuarios/IngresoDatos',
+                //url: 'http://localhost:58924/api/usuarios/IngresoDatos',
                 dataType: 'json',
                 data: JSON.stringify(usuarioRequest),
                 contentType: 'application/json; charset=utf-8',
